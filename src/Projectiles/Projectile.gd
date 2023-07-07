32c4c57797500183dde645a68d77baadda850602
Bohdan <bodya.ru02@gmail.com>
2023-07-08 02:21:06 +0300

    Add basis for death

--- src/Projectiles/Projectile.gd
+++ src/Projectiles/Projectile.gd
@@ -7,6 +7,7 @@ const DAMAGE_COEFFICIENT = 0.1
 var damage = base_damage + speed * DAMAGE_COEFFICIENT
 var direction = Vector2.RIGHT
 var size = 1.0
+var is_dead = false
 
 # For calculating trajectory based on elapsed time
 var elapsed_time = 0
@@ -20,6 +21,7 @@ func _process(delta):
 func _physics_process(delta):
 	update_position(delta)
 	handle_logic()
+	handle_death()
 
 func set_direction(dir: Vector2):
 	direction = dir
@@ -29,3 +31,9 @@ func update_position(delta):
 
 func handle_logic():
 	pass
+
+func handle_death():
+	pass
+
+func die():
+	is_dead = true
