68bd80058b9e8b20f2887939729815c01d079c15
alorthius <alinium3012@gmail.com>
2023-07-08 08:54:56 +0300

    clean up

--- src/GameField/MainScene.gd
+++ src/GameField/MainScene.gd
@@ -1,7 +1,6 @@
 extends Control
 
-@onready var _projectiles_bar = $ProjectilesBar as Control
-@onready var _projectiles_butts = $ProjectilesBar/VBox.get_children()
+@onready var _projectiles_butts = $VBoxLeft.get_children()
 
 @onready var _aim_rect = $AimField.get_rect() as Rect2
 @onready var _ai_rect = $AiField.get_rect() as Rect2
@@ -15,7 +14,7 @@ func _ready():
 
 
 func _on_projectile_butt_pressed(butt):
-	_preview = load("res://src/UI/ProjectilePreview.tscn").instantiate()
+	_preview = load("res://src/ProjectilesLauncher/Launcher.tscn").instantiate()
 	$CanvasLayer.add_child(_preview)
 	_preview.spawn_projectile.connect(_spawn_projectile)
 	_preview.init_set(butt.projectile_scene, butt.projectile_texture, _aim_rect, _ai_rect)
