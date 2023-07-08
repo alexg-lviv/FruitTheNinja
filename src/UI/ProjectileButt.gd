5666ed4c16e5d7cc9ef59c90e1d9acd1116d6d7e
alorthius <alinium3012@gmail.com>
2023-07-08 03:37:40 +0300

    Kokoko

--- src/UI/ProjectileButt.gd
+++ src/UI/ProjectileButt.gd
@@ -1,4 +1,11 @@
 extends TextureButton
 
-@onready var projectile_scene = "res://src/Projectiles/Projectile.tscn"
-@onready var projectile_texture = $Icon.texture
+@export var projectile: String
+
+var projectile_texture
+var projectile_scene: String
+
+func _ready():
+	projectile_scene = "res://src/Projectiles/" + projectile + "/" + projectile + ".tscn"
+	$Icon.texture = load("res://assets/fruits/" + projectile + ".png")
+	projectile_texture = $Icon.texture
