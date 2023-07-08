a48a4ac363f0d59f4554d5222f8b5c6dd3a8ff7b
Bohdan <bodya.ru02@gmail.com>
2023-07-09 00:14:07 +0300

    Add crash particles for watermelon

--- src/Projectiles/Watermelon/Watermelon.gd
+++ src/Projectiles/Watermelon/Watermelon.gd
@@ -2,12 +2,17 @@ extends "res://src/Projectiles/Projectile.gd"
 
 class_name Watermelon
 
+const MAX_BOUNCE_COUNT = 5
+
+@onready var CrashParticles = $CrashParticles
+
 func _ready():
 	super._ready()
+	CrashParticles.emitting = false
 	color = Color(0.990, 0.109, 0.138)
 
 func handle_logic():
-	if bounce_count >= 5:
+	if bounce_count >= MAX_BOUNCE_COUNT:
 		die()
 
 func _on_area_entered(area: Area2D):
@@ -18,7 +23,11 @@ func _on_area_entered(area: Area2D):
 			"Top", "Bottom":
 				Signals.emit_signal("camera_shake_requested", 12.0, 0.5)
 				direction.y *= -1
+				if bounce_count < MAX_BOUNCE_COUNT:
+					CrashParticles.restart()
 			"Left", "Right":
 				Signals.emit_signal("camera_shake_requested", 12.0, 0.5)
 				direction.x *= -1
 				rotation_speed *= -1
+				if bounce_count < MAX_BOUNCE_COUNT:
+					CrashParticles.restart()
