724f7e6a7e0c169ac3528aa540ad4c0ef50a2b6f
Adeon18 <ostaptrush18@gmail.com>
2023-07-09 13:22:59 +0300

    Added decal on watermelon collision

--- src/Effects/DecalSystem.gd
+++ src/Effects/DecalSystem.gd
@@ -8,12 +8,18 @@ func add_decals(global_pos: Vector2, color: Color, size: float):
 	
 	var sizes = 0.0035 * (2 + size) / 2.0
 	for i in rng.randf_range(1, 4):
-		var decalIns = DecalObj.instantiate()
-		decalIns.set_lifetime(rng.randf_range(15, 30))
-		decalIns.set_color(color)
-		var scale = sizes
-		decalIns.scale = Vector2(scale, scale)
-		add_child(decalIns)
-		decalIns.global_position = global_pos + Vector2(rng.randf_range(-32, 32), rng.randf_range(-32, 32))
+		add_decal(global_pos, color, sizes)
 		
 		sizes -= randf_range(0.002, sizes / 2.0)
+
+
+func add_decal(global_pos: Vector2, color: Color, size: float):
+	var sizes = 0.0035 * (2 + size) / 2.0
+	var decalIns = DecalObj.instantiate()
+	decalIns.set_lifetime(rng.randf_range(15, 30))
+	decalIns.set_color(color)
+	var scale = sizes
+	decalIns.scale = Vector2(scale, scale)
+	add_child(decalIns)
+	decalIns.global_position = global_pos + Vector2(rng.randf_range(-32, 32), rng.randf_range(-32, 32))
+	
