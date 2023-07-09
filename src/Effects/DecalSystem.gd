f5bb20d2c1d4478531f7bbe3ec88004738d0f689
Adeon18 <ostaptrush18@gmail.com>
2023-07-09 13:50:40 +0300

    Edited decals

--- src/Effects/DecalSystem.gd
+++ src/Effects/DecalSystem.gd
@@ -9,7 +9,7 @@ func add_decals(global_pos: Vector2, color: Color, size: float):
 	var sizes = 0.0035 * (2 + size) / 2.0
 	for i in rng.randf_range(1, 4):
 		var decalIns = DecalObj.instantiate()
-		decalIns.set_lifetime(rng.randf_range(15, 30))
+		decalIns.set_lifetime(rng.randf_range(60, 120))
 		decalIns.set_color(color)
 		var scale = sizes
 		decalIns.scale = Vector2(scale, scale)
