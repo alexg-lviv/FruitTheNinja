9ffa596ebcc296a950ec7452fd7af887b62e355a
alorthius <alinium3012@gmail.com>
2023-12-15 11:36:51 +0200

    add models

--- microservices/DataManager/models/lstm.py
+++ microservices/DataManager/models/lstm.py
@@ -18,7 +18,7 @@ class LSTMModel(nn.Module):
             hidden_size=hidden_size,
             num_layers=num_layers,
             batch_first=True,
-            dropout = 0.25
+            dropout=0.25
         )
 
         self.fc = nn.Linear(hidden_size, num_classes)
@@ -38,4 +38,23 @@ class LSTMModel(nn.Module):
         out = self.fc(h_out)
         out = self.dropout(out)
 
+        return out
+
+
+class LSTM2(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes, num_layers):
+        super(LSTM2, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True, dropout=0.5)
+        self.fc1 = nn.Linear(hidden_size * 2, 32)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(32, num_classes)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1, :]  # Take the output from the last time step
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
         return out
